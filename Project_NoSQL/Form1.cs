0c1d73357d5412649f378ce93f7a24ab08e417f0
Yornie Westink <jinxie88@live.nl>
2021-09-14 10:33:28 +0200

    Init Project
    - Start of the project
    - Boilerplate code
    - connection test

---
+++ Project_NoSQL/Form1.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_NoSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void TestConnection()
+        {
+            /*var connection = MongoClientSettings.FromConnectionString(ConfigurationManager.ConnectionStrings["Mongo"].ConnectionString);
+            MongoClient mongoClient = new MongoClient(connection);
+            var dblist = mongoClient.ListDatabases().ToList();
+
+            Console.WriteLine("the list of databases on this server is: ");
+            foreach (var db in dblist)
+            {
+                Console.WriteLine(db);
+            }
+            */
+        }
+
+    }
+}
